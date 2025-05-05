7b1838dc694ad4491064cdc2d87ca54abf224cdc
Pedro Henrique Leite Da Costa <pedrohenriqueleiteda.costa@philips.com>
2025-05-05 14:50:17 -0300

    Primeiro commit

---
+++ dog.java
@@ -0,0 +1,15 @@
+public class dog(){
+
+    private String nome;
+    private int idade;
+    private char genero;
+
+    public void latir(){
+        system.out.prinln("AU AU!");
+    }
+
+    public void correr(){
+        system.out.prinln("O cachorro correu");
+    }
+
+}
