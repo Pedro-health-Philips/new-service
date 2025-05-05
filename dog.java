931f02330c554cae76ef6a3941bd7e78c0c9fedf
Pedro Henrique Leite Da Costa <pedrohenriqueleiteda.costa@philips.com>
2025-05-05 15:44:46 -0300

    Comitao 2

--- dog.java
+++ dog.java
@@ -5,31 +5,9 @@ public class Dog(){
     private char genero;
 
     public void comer(){
-        system.out.prinln("Cachorro comeu ração");
+        system.out.prinln("Cachorro comeu");
     }
 
-    public String getNome() {
-        return nome;
-    }
-    
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    public int getIdade() {
-        return idade;
-    }
-    
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    
-    public char getGenero() {
-        return genero;
-    }
-    
-    public void setGenero(char genero) {
-        this.genero = genero;
-    }
+   
 
 }
