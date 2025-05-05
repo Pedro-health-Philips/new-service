df0f938b3efcfe69b57e220ad5ea7a61a7543610
Pedro Henrique Leite Da Costa <pedrohenriqueleiteda.costa@philips.com>
2025-05-05 15:41:05 -0300

    Commitao

--- dog.java
+++ dog.java
@@ -1,4 +1,4 @@
-public class dog(){
+public class Dog(){
 
     private String nome;
     private int idade;
@@ -6,6 +6,30 @@ public class dog(){
 
     public void comer(){
         system.out.prinln("Cachorro comeu ração");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public char getGenero() {
+        return genero;
+    }
+    
+    public void setGenero(char genero) {
+        this.genero = genero;
     }
 
 }
