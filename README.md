02bfc563a713edfa46b8b7b6895aebd5b5045fca
Pedro Henrique Leite Da Costa <pedrohenriqueleiteda.costa@philips.com>
2025-05-06 08:55:00 -0300

    Deu boa chama o gremiop

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # new-service
 Repositório criado por conta da atividade 2 do treinamento de GitHub
+# Updates
+Nesse update irei criar uma classe java para a branch
--- dog.java
+++ dog.java
@@ -4,10 +4,40 @@ public class Dog(){
     private int idade;
     private char genero;
 
+    public String mover(){
+    }
+
+    
     public void comer(){
         system.out.prinln("Cachorro comeu");
     }
 
-   
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    public char getGenero() {
+        return genero;
+    }
+    
+    public void setGenero(char genero) {
+        this.genero = genero;
+ 
 
+}
+
+
+   
 }
